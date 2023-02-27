6fec5bc1945fcf56979cc697bb6ba8d321672b77
ReZeM <rezem2197@gmail.com>
2023-02-27 09:54:36 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Refine } from "@pankod/refine-core";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+import { HeadlessInferencer } from "@pankod/refine-inferencer/headless";
+
+const App = () => {
+  return (
+    <Refine
+      routerProvider={routerProvider}
+      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      resources={[
+        {
+          name: "products",
+          list: HeadlessInferencer,
+          show: HeadlessInferencer,
+          create: HeadlessInferencer,
+          edit: HeadlessInferencer,
+        },
+      ]}
+    />
+  );
+};
+export default App;
